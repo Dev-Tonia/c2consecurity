44cad49f78b5ebe4e251f27a0aaef0cd5a902b71
Dev-Tonia <ogunwaanthonia@gmail.com>
2024-01-11 23:12:24 +0100

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <nav class="flex justify-between">
+      <div>
+        <NuxtLink to="/">Security</NuxtLink>
+      </div>
+
+      <ul class="flex justify-between items-center">
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/service">Service</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/jobs">Jobs</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/contact-us">Contact us</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <slot />
+  </main>
+</template>
+
+<script setup></script>
+
+<style scoped>
+li {
+  @apply px-2 text-2xl font-medium;
+}
+</style>
---
+++ pages/jobs.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2 class="text-center text-red-700 font-bold">Jobs page You Know</h2>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
