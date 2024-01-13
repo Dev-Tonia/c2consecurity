d3eca781c8ecbab8b11af3d60fb17d4298d3c32a
Dev-Tonia <ogunwaanthonia@gmail.com>
2024-01-13 15:39:24 +0100

    made some changes of the footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -29,7 +29,7 @@
       <div class="container">
         <div class="grid grid-cols-4 gap-4">
           <div class="col-span-2">
-            <h1 class="font-bold text-2xl">
+            <h1 class="font-bold text-2xl mb-6">
               c2constructionsecurityservicesltd
             </h1>
             <p class="text-xs md:text-sm">
@@ -40,44 +40,46 @@
             </p>
           </div>
           <div>
-            <h1 class="font-bold text-2xl">Services</h1>
+            <h1 class="font-bold text-2xl mb-6">Services</h1>
             <ul>
-              <li class="text-xs">
+              <li class="text-xs mb-2">
                 <NuxtLink to="service"
                   >SECURITY GUARDING - ALL SECTORS</NuxtLink
                 >
               </li>
-              <li class="text-xs">
+              <li class="text-xs mb-2">
                 <NuxtLink to="service">ID CARD SYSTEMS</NuxtLink>
               </li>
-              <li class="text-xs">
+              <li class="text-xs mb-2">
                 <NuxtLink to="service"> KEY HOLDING SERVICES </NuxtLink>
               </li>
-              <li class="text-xs">
+              <li class="text-xs mb-2">
                 <NuxtLink to="service"> EVENT MANAGEMENT </NuxtLink>
               </li>
-              <li class="text-xs">
+              <li class="text-xs mb-2">
                 <NuxtLink to="service">
                   IP & ANALOGUE CCTV INSTALLATION
                 </NuxtLink>
               </li>
-              <li class="text-xs">
+              <li class="text-xs mb-2">
                 <NuxtLink to="service"> BIO METRIC ACCESS CONTROL </NuxtLink>
               </li>
             </ul>
           </div>
           <div>
-            <h1 class="font-bold text-2xl">Contact Details</h1>
-            <h5 class="font-bold">Head Office</h5>
-            <p class="text-xs">20-22 Wenlock Road London N1 7GU</p>
-            <div class="text-xs">
-              <span class="font-bold">Tel:</span>020 7117 2469
+            <h1 class="font-bold text-2xl mb-6">Contact Details</h1>
+            <div class="mb-2">
+              <h5 class="font-bold">Head Office</h5>
+              <p class="text-xs">20-22 Wenlock Road London N1 7GU</p>
             </div>
-            <div class="text-xs">
+            <div class="text-xs mb-2">
+              <span class="font-bold">Tel:</span> 020 7117 2469
+            </div>
+            <div class="text-xs mb-2">
               <span class="font-bold">Mob:</span>
               07949 898649
             </div>
-            <div class="text-xs">
+            <div class="text-xs mb-2">
               <span class="font-bold">Email:</span>
               info@prolinksecurity.co.uk
             </div>
