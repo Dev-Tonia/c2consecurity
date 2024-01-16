bf1e3920faa964a69b4368c7e37c81ab6995d9a0
Dev-Tonia <ogunwaanthonia@gmail.com>
2024-01-16 22:03:39 +0100

    added the google map for location

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="h-64 bg-[url('~/assets/img/banner.jpg')] bg-no-repeat bg-[length:100%_90%] relative"
+    class="h-64 bg-[url('~/assets/img/banner.jpg')] bg-no-repeat bg-[length:100%_100%] relative"
   >
     <div
       class="absolute bg-black/60 top-0 bottom-0 right-0 left-0 flex items-center justify-center"
